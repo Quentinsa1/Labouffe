344adff0f638b63a4dce75760012d98d4a4bbe80
alix <audreymotso@gmail.com>
2024-12-04 11:06:25 +0100

    acceuil3

--- lib/screen/commandeReguliereAcceuil.dart
+++ lib/screen/commandeReguliereAcceuil.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'detailsRepas.dart';
 
 
@@ -13,53 +12,62 @@ class listRepasRegulier extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
-      itemCount: products.length,
-      separatorBuilder: (context, index) => const Divider(
-        height: 1,
-        color: Colors.grey,
-      ),
-      itemBuilder: (context, index) {
-        final product = products[index];
+    return
+      Expanded(
+        child: ListView.separated(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: products.length,
+          separatorBuilder: (context, index) => const SizedBox(height: 8),
+          itemBuilder: (context, index) {
+            final product = products[index];
 
-        return ListTile(
-          leading: ClipRRect(
-            borderRadius: BorderRadius.circular(8),
-            child: Image.network(
-              product['image'],
-              width: 50,
-              height: 50,
-              fit: BoxFit.cover,
-            ),
-          ),
-          title: Text(
-            product['name'],
-            style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-          subtitle: Text(
-            product['price'],
-            style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-              color: Colors.grey[700],
-            ),
-          ),
-          trailing: const Icon(
-            Icons.arrow_forward_ios,
-            size: 16,
-          ),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => detailsRepas(product: product),
+            return Card(
+              elevation: 4,
+              margin: const EdgeInsets.symmetric(horizontal: 16),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: ListTile(
+                contentPadding: const EdgeInsets.all(8),
+                leading: ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.network(
+                    product['image'],
+                    width: 50,
+                    height: 50,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                title: Text(
+                  product['name'],
+                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                    fontWeight: FontWeight.w600,
+                   // color: Colors.black,
+                  ),
+                ),
+                subtitle: Text(
+                  product['price'],
+                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                   // color: Colors.black,
+                  ),
+                ),
+                trailing: const Icon(
+                  Icons.arrow_forward_ios,
+                  size: 16,
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => detailsRepas(product: product),
+                    ),
+                  );
+                },
               ),
             );
           },
-        );
-      },
-    );
+        ),
+      );
   }
 }
